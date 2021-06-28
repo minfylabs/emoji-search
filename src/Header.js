72caa4da04298b2e149aa72f34f6a23202777f8d
Bharath Kumar <bharath.dtu@gmail.com>
2021-06-28 16:06:57 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends PureComponent {
           height="32"
           alt=""
         />
-        Emoji Search V2
+        Emoji Search V3
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
           width="32"
